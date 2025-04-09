c80bad31574113b04a2c0bf175931a92571352e5
monomixs <wedleygouverneur14@gmail.com>
2025-04-08 21:11:16 -0400

    Delete migrate_db.py

--- migrate_db.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-
-with app.app_context():
-    # Drop existing tables
-    db.drop_all()
-    
-    # Recreate the tables with the updated schema
-    db.create_all()
-    print("Database tables dropped and recreated successfully")
